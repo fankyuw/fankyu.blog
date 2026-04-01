92bf6271342e68e5dc18cd217e5ec920853e4310
fankyu <fankydoesart@gmail.com>
2026-04-01 02:35:17 -0300

    COMMIT

---
+++ art/logic.js
@@ -0,0 +1,32 @@
+var imgs = []
+var imgsExt = ["png","jpg","jpeg","webp"]
+
+var assetPath = "/art/randomart/"
+
+const imgNode = document.createElement("img")
+imgNode.setAttribute("class","img")
+
+$.ajax({
+    // type: "method",
+    url: assetPath,
+    // dataType: "dataType",
+    success: function (response) {
+        imgs = []
+        imgsExt.forEach(element => {
+            $(response).find(`a:contains(.${element})`).each(function(){
+                imgs.push($(this).attr("href"))
+            })
+        })
+        imgs.forEach(element => {
+            const currentImg = document.getElementById('container1').appendChild(imgNode.cloneNode())
+            console.log(element)
+            var newText = element.replaceAll("%5C","/")
+            var newPath = newText.replaceAll("/assets","")
+            if (window.location.hostname == "fankyu.blog") {
+                currentImg.setAttribute("src",newText)
+            }else {
+                currentImg.setAttribute("src",newText)
+            }
+        })
+    }
+});
