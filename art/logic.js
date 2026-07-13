eb0e032032ddd8e91cc101bb10000d6842b30f16
fankyu <fankydoesart@gmail.com>
2026-07-12 21:45:46 -0400

    made zoom better BUT it doesn't work on small screens :( FIX LATER!!!

--- art/logic.js
+++ art/logic.js
@@ -7,16 +7,78 @@ const imgNode = document.createElement("div")
 imgNode.setAttribute("class","img")
 const popinNode = document.createElement("div")
 popinNode.setAttribute("class","popin")
+const modal = document.querySelector(".modal");
+
+var x = 0
+var y = 0
+
+const modalimg = document.getElementById("modalImage");
 
 document.getElementById("themodal").addEventListener("click", function() {
     const topElement = document.elementFromPoint(event.clientX, event.clientY);
     if (!topElement.classList.contains("modal-image")) {
-        const modal = document.querySelector(".modal");
         modal.style.display = "none";
+        zoom = false
+        fixModalSize()
+    }
+})
+function fixModalSize() {
+    modalimg.style.height = window.innerHeight * 0.95 + "px";
+    modalimg.style.marginTop = window.innerHeight * 0.025 + "px";
+    if (window.innerWidth * 0.8 < parseInt(modalimg.style.height)) {
+        console.log("width is smaller than height")
+        modalimg.style.height = window.innerWidth * 0.8 + "px";
+    }
+    if (zoom) {
+        modalimg.style.marginTop = 0;
+        modalimg.style.height = "auto";
+        modalimg.style.maxWidth = "auto";
+        modalimg.style.top = "2.5%";
+    }
+}
+
+fixModalSize()
+window.addEventListener("resize", function() {
+    fixModalSize()
+})
+var zoom = false
+
+modalimg.addEventListener("mouseover", function() {
+    if (!zoom) {
+        modalimg.style.cursor = "zoom-in"
+    } else {
+        modalimg.style.cursor = "zoom-out"
     }
 })
 
+document.addEventListener("mousemove", function(event) {
+    y = event.clientY
+    x = event.clientX
+})
 
+
+
+
+modalimg.addEventListener("click", function() {
+    if (!zoom) {
+        console.log(window.innerHeight)
+        console.log(modalimg.scrollHeight)
+        modalimg.style.cursor = "zoom-out"
+        modalimg.style.height = "auto";
+        modalimg.style.maxWidth = "auto";
+        modalimg.style.top = "2.5%";
+        modalimg.style.marginTop = 0;
+        modal.scrollTop = modalimg.scrollHeight * (y / window.innerHeight)/2;
+        console.log(modalimg.scrollTop)
+        console.log(modalimg.scrollHeight * (y / window.innerHeight)/2)
+      
+        zoom = true
+    } else {
+        modalimg.style.cursor = "zoom-in"
+        zoom = false
+        fixModalSize()
+    }
+})
 
 $.ajax({
     // type: "method",
